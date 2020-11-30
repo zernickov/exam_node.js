998caf1b45b02f0e65bcd1bda8c90d643a97aa6e
Patrick <50173111+PatrickDhamiKEA@users.noreply.github.com>
2020-11-30 22:50:39 +0100

    renset lidt

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const mysql = require("mysql");
-const app = express();
+const rateLimiter = require("express-rate-limit");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
 const session = require("express-session");
 require("dotenv").config();
+const app = express();
+const saltRounds = 10;
+const nodemailer = require("nodemailer");
 
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "robogpat@gmail.com",
+        pass: "robogpat123"
+    }
+});
+
+const authLimiter = rateLimiter({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // limit each IP to 6 requests per windowMs
+});
+
+app.use(authLimiter);
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,35 +35,15 @@ app.use(session({
         secure: false }
 }));
 
-const rateLimiter = require("express-rate-limit");
-
-const authLimiter = rateLimiter({
-    windowMs: 10 * 60 * 1000, // 10 minutes
-    max: 100 // limit each IP to 6 requests per windowMs
-});
-
-app.use(authLimiter);
-
-const nodemailer = require("nodemailer");
-
-let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "robogpat@gmail.com",
-        pass: "robogpat123"
-    }
-});
-
-function mailSender(mail, name){
+function mailSender(mail, name) {
     let mailOptions = {
         from: "robogpat@gmail.com",
         to: mail,
         subject: "Welcome",
         text: `Hello ${name} welcome to the app`
     };
-
-    transporter.sendMail(mailOptions, function(err, data) {
-        if (err){
+    transporter.sendMail(mailOptions, (err) => {
+        if (err) {
             console.log(err);
         } else {
             console.log("Email Sent!!");
@@ -55,11 +51,8 @@ function mailSender(mail, name){
     });
 }
 
-
-
 const redirectLogin = (req, res, next) => {
-  if (!req.session.userId){
-      console.log(req.session.userId);
+  if (!req.session.userId) {
     res.redirect("/login");
   } else {
       next();
@@ -67,8 +60,7 @@ const redirectLogin = (req, res, next) => {
 };
 
 const redirectHome = (req, res, next) => {
-    if (req.session.userId){
-        console.log(req.session.userId);
+    if (req.session.userId) {
         res.redirect("/");
     } else {
         next();
@@ -76,7 +68,7 @@ const redirectHome = (req, res, next) => {
 };
 
 const redirectAway = (req, res, next) => {
-  if (!req.session.userId){
+  if (!req.session.userId) {
       res.send("<h1>Unauthorized</h1>");
   } else {
       next();
@@ -84,10 +76,8 @@ const redirectAway = (req, res, next) => {
 };
 
 app.get("/", redirectLogin, (req, res) => {
-    // const { userId } = req.session;
     res.sendFile(__dirname + "/public/home_page/home_page.html")
 });
-
 
 app.get("/login", redirectHome, (req, res) => {
     res.sendFile(__dirname + "/public/login_page/login_page.html")
@@ -108,30 +98,25 @@ const connection = mysql.createConnection({
 });
 
 connection.connect();
-
 connection.query(`USE bluekite3;`);
-
 // connection.query("DROP TABLE users;");
-
 // connection.query(`CREATE TABLE users (user_id INT NOT NULL AUTO_INCREMENT, username VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(user_id));`);
 
-
 app.post("/register", (req, res) => {
-    bcrypt.genSalt(saltRounds, function (saltErr, salt) {
+    bcrypt.genSalt(saltRounds,(saltErr, salt) => {
         if (saltErr) {
             console.log("SALT ERROR:", saltErr);
         }
-        bcrypt.hash(req.body.password, salt, function (hashErr, hash) {
+        bcrypt.hash(req.body.password, salt, (hashErr, hash) => {
             if (hashErr) {
                 console.log("HASH ERROR:", hashErr);
             }
-            connection.query(`INSERT INTO users (username, email, password) VALUES (?, ?, ?);`, [req.body.username, req.body.email, hash], function (dbErr) {
+            connection.query(`INSERT INTO users (username, email, password) VALUES (?, ?, ?);`, [req.body.username, req.body.email, hash], (dbErr) => {
                 if (dbErr) {
                     console.log("DATABASE ERROR:", dbErr);
-                }
-                else {
+                } else {
                     mailSender(req.body.email, req.body.username);
-                    connection.query(`SELECT user_id FROM users WHERE username=?`, req.body.username, function (err, result) {
+                    connection.query(`SELECT user_id FROM users WHERE username=?`, req.body.username, (err, result) => {
                         const userid = JSON.parse(JSON.stringify(result[0].user_id));
                         req.session.userId = userid;
                         res.redirect("/");
@@ -143,25 +128,26 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-    connection.query(`SELECT * FROM users WHERE username=?;`, req.body.username, function (err, result) {
-        const userid = JSON.parse(JSON.stringify(result[0].user_id));
-        console.log(userid);
-        const hashed = JSON.parse(JSON.stringify(result[0].password));
-        bcrypt.compare(req.body.password, hashed, function (err, result1) {
+    connection.query(`SELECT * FROM users WHERE username=?;`, req.body.username, (err, result) => {
+        try {
+            const userid = JSON.parse(JSON.stringify(result[0].user_id));
+            const hashed = JSON.parse(JSON.stringify(result[0].password));
+
+        bcrypt.compare(req.body.password, hashed, (err, result1) => {
             if (err) {
                 console.log("ERROR:", err);
-            }
-            else if (result1) {
+            } else if (result1) {
                 console.log("SUCCESS:", result1);
                 req.session.userId = userid;
                 console.log("session: ", req.session.userId);
                 res.redirect("/");
-            }
-            else {
+            } else {
                 console.log("SOMETHING ELSE WENT WRONG, RES:", result1);
             }
         })
-    });
+    } catch (Exception){
+            res.redirect("/");
+        }});
 });
 
 app.post("/logout", (req, res) => {
@@ -169,15 +155,16 @@ app.post("/logout", (req, res) => {
     res.redirect("/");
 });
 
-connection.query(`SELECT * FROM users;`, (error, result, fields) => {
+connection.query(`SELECT * FROM users;`, (error, result) => {
     console.log(result);
 });
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, (error => {
+app.listen(port, (error) => {
     if (error) {
         console.log("Server couldn't start:", error);
     }
     console.log("Server started on port:", Number(port));
-}));
+});
+
